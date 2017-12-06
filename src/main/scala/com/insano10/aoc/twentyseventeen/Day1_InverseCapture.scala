5e8e06ea13a06e0e6d0cb38f71f1c792a0194066
jennifer.beckett <jennifer.beckett@ovoenergy.com>
2017-12-06 18:25:47 +0000

    01/12/2017 part 2

--- src/main/scala/com/insano10/aoc/twentyseventeen/Day1_InverseCapture.scala
+++ src/main/scala/com/insano10/aoc/twentyseventeen/Day1_InverseCapture.scala
@@ -1,18 +1,24 @@
 package com.insano10.aoc.twentyseventeen
 
+import scala.annotation.tailrec
+
 class Day1_InverseCapture {
 
+  private def getValue(num1: Int, num2: Int): Long = {
+
+    if(num1 == num2) {
+      num1
+    } else {
+      0
+    }
+  }
+
+  /*
+  shuffle digits between left/right list to compare each digit with neighbour
+   */
   def getCaptureSum(capture: String): Long = {
 
-    def getValue(num1: Int, num2: Int): Long = {
-
-      if(num1 == num2) {
-        num1
-      } else {
-        0
-      }
-    }
-
+    @tailrec
     def sumAndRecurse(leftInput: List[Int], rightInput: List[Int], totalSum: Long): Long = {
 
       rightInput match {
@@ -23,5 +29,29 @@ class Day1_InverseCapture {
 
     val captureDigits = capture.toList.map(_.asDigit)
     sumAndRecurse(List(captureDigits.head), captureDigits.tail, 0)
+  }
+
+  /*
+  duplicate the digits into one list to avoid wrapping then move through comparing with the offset digit
+   */
+  def getCaptureSumPart2(capture: String): Long = {
+
+    @tailrec
+    def sumAndRecurse(input: List[Int], comparisonOffset: Int, digitsRemaining: Int, totalSum: Long): Long = {
+
+      val nextVal = getValue(input.head, input.drop(comparisonOffset).head)
+
+      if(digitsRemaining == 1) {
+        totalSum + nextVal
+      } else {
+        sumAndRecurse(input.tail, comparisonOffset, digitsRemaining - 1, totalSum + nextVal)
+      }
+    }
+
+    val numDigits = capture.length
+    val comparisonOffset = numDigits/2
+    val doubleCaptureDigits = (capture ++ capture).toList.map(_.asDigit)
+
+    sumAndRecurse(doubleCaptureDigits, comparisonOffset, numDigits, 0)
   }
 }
