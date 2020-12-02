8010fc82bd0299cf9c39aa3129398fbfa56cd4ff
insano10 <jdommett@gmail.com>
2020-12-02 14:28:34 +0000

    solve day 1 part 1

--- 2020/advent-of-code/src/advent_of_code/day_1_report_repair.clj
+++ 2020/advent-of-code/src/advent_of_code/day_1_report_repair.clj
@@ -12,16 +12,12 @@
                       flatten
                       (map #(Integer/parseInt %))
                       sort)]
-    (println contents)
     (loop [first-num (first contents)
            others (rest contents)]
-      (let [candidate (->> others
-                           (take-while #(<= (+ first-num %) 2020))
-                           last)]
-        (println (str "candidate = " candidate))
+      (when-let [candidate (->> others
+                                (take-while #(<= (+ first-num %) 2020))
+                                last)]
         (if (= 2020 (+ first-num candidate))
           [(str first-num " + " candidate " = 2020")
            (str first-num " * " candidate " = " (* first-num candidate))]
-          (do
-            (println (str (+ first-num candidate) " not equal"))
-            (recur (first others) (rest others))))))))
+          (recur (first others) (rest others)))))))
