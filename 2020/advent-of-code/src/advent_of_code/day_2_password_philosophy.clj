a4c48de6f8768ec4ea25ae7f7d8b0e1e5bc63534
insano10 <jdommett@gmail.com>
2020-12-02 20:28:10 +0000

    solve day 2 part 2

--- 2020/advent-of-code/src/advent_of_code/day_2_password_philosophy.clj
+++ 2020/advent-of-code/src/advent_of_code/day_2_password_philosophy.clj
@@ -12,7 +12,7 @@
      :max      (Integer/parseInt (last occurences))
      :password (nth sections 2)}))
 
-(defn- is-valid? [policy]
+(defn- is-valid-pt1? [policy]
   (let [password (:password policy)
         matching-chars (filter #(= (:char policy) %) (str/split password #""))]
     (and (>= (count matching-chars) (:min policy))
@@ -24,5 +24,24 @@
   (let [contents (->> (utils/read-csv csv-filename)
                       flatten
                       (map ->policy))
-        valid-passwords (filter is-valid? contents)]
+        valid-passwords (filter is-valid-pt1? contents)]
+    (count valid-passwords)))
+
+
+
+(defn- is-valid-pt2? [policy]
+  (let [password (str/split (:password policy) #"")
+        first-char (nth password (dec (:min policy)))
+        second-char (nth password (dec (:max policy)))
+        target-char (:char policy)]
+    (= 1 (->> [first-char second-char]
+              (filter #(= target-char %))
+              count))))
+
+;; (day2/solve-part2 "resources/day2_example.csv")
+(defn solve-part2 [csv-filename]
+  (let [contents (->> (utils/read-csv csv-filename)
+                      flatten
+                      (map ->policy))
+        valid-passwords (filter is-valid-pt2? contents)]
     (count valid-passwords)))
