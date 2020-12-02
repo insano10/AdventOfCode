ca10721c57e2b04492520749468b3bf78befd174
insano10 <jdommett@gmail.com>
2020-12-02 14:25:52 +0000

    solve day 1 example

--- 2020/advent-of-code/src/advent_of_code/day_1_report_repair.clj
+++ 2020/advent-of-code/src/advent_of_code/day_1_report_repair.clj
@@ -1,4 +1,27 @@
-(ns advent-of-code.day-1-report-repair)
+(ns advent-of-code.day-1-report-repair
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
 
-(defn foo []
-  (println "day 1"))
+(defn- read-csv [input-file]
+  (with-open [reader (io/reader input-file)]
+    (doall (csv/read-csv reader))))
+
+;; (day1/solve "resources/day1_example.csv")
+(defn solve [csv-filename]
+  (let [contents (->> (read-csv csv-filename)
+                      flatten
+                      (map #(Integer/parseInt %))
+                      sort)]
+    (println contents)
+    (loop [first-num (first contents)
+           others (rest contents)]
+      (let [candidate (->> others
+                           (take-while #(<= (+ first-num %) 2020))
+                           last)]
+        (println (str "candidate = " candidate))
+        (if (= 2020 (+ first-num candidate))
+          [(str first-num " + " candidate " = 2020")
+           (str first-num " * " candidate " = " (* first-num candidate))]
+          (do
+            (println (str (+ first-num candidate) " not equal"))
+            (recur (first others) (rest others))))))))
